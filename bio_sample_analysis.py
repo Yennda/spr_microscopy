53ed92cebb4ea39c4d5e47165864576229c243e3
Bukáček Jan <Bukacek@ufe.cz>
2020-01-30 08:14:08 +0100

    aaa

--- bio_sample_analysis.py
+++ bio_sample_analysis.py
@@ -6,19 +6,19 @@ tl.clear_all()
 plt.close("all")
 
 main_folder='C:/SPRUP_data_Jenda/2019_03_13_Jenda_microscopy/'
-folder=main_folder+'19_12_04_second_poc/'
-#folder=main_folder+'20_01_24_third/'
+#folder=main_folder+'19_12_04_second_poc/'
+folder=main_folder+'20_01_24_third/'
 
 
 file = 'raw_01'
-file = 'raw_03'
+#file = 'raw_03'
 
 video = BioVideo(folder, file, 1)
 video._channels=[0]
 video.loadData()
 #video.ref_frame=-1
-#video.make_toggle()
-video.make_diff()
+video.make_toggle()
+#video.make_diff()
 #video.make_both()
 
 #video.ref_frame = 0
