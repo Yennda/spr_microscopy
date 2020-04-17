13ef6f291c5dccc8eec9f4fe3a30e249c0be1da1
Bukáček Jan <Bukacek@ufe.cz>
2020-04-17 14:48:42 +0200

    aaa

--- global_var.py
+++ global_var.py
@@ -5,12 +5,13 @@ FOLDER_BIOEXPORTS = '/exports_bio'
 NAME_LOCAL_SPR = 'spr'
 NAME_GLOBAL_SPR = 'spr_integral'
 
-yellow='#ffb200'
-red='#DD5544'
-blue='#0284C0'
-black='#000000'    
-green='#008000'
-gray='#555555'
+yellow = '#ffb200'
+red = '#DD5544'
+blue = '#0284C0'
+black = '#000000'    
+green = '#008000'
+gray = '#555555'
+purple = '#DD84C0'
 
 COLORS = [yellow, blue, red, black]
 
