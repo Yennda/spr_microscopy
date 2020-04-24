1854f291d1b971660f0dc7f2a4af8e0e115a14ab
Bukáček Jan <Bukacek@ufe.cz>
2020-04-24 15:26:43 +0200

    aaa

--- sample_analysis_img_processing.py
+++ sample_analysis_img_processing.py
@@ -39,14 +39,14 @@ file = 'raw_27_1'
 #folder=main_folder+'20_04_03_L3/'
 #file = 'raw_01_1'
 
-folder=main_folder+'20_03_23_L3_4x/'
-file = 'raw_06_1'
+#folder=main_folder+'20_03_23_L3_4x/'
+#file = 'raw_06_1'
 
-#folder=main_folder+'20_04_14_M5/'
-#file = 'raw_11_1'
+folder=main_folder+'20_04_14_M5/'
+file = 'raw_28_1'
 #
-folder=main_folder+'20_04_15_L3/'
-file = 'raw_17_1'
+#folder=main_folder+'20_04_15_L3/'
+#file = 'raw_17_1'
 #
 #folder=main_folder+'20_04_20_Q4/'
 #file = 'raw_22_1'
@@ -56,7 +56,7 @@ video = Video(folder, file)
 video.loadData()
 
 video._video['raw']=video._video['raw'][100:,430:730,:]
-video.make_diff(k = 10)
+#video.make_diff(k = 10)
 #video.change_fps(10)
 #video.make_int(k = 10)
 video.fouriere(level = 20)
@@ -66,17 +66,18 @@ video.fouriere(level = 20)
 #video.img_process_alpha(threshold = 2.5, noise_level = 0.0012U)
 #video.characterize_nps(save = False)
 #video.exclude_nps([3], exclude = True)
-video.make_toggle(['diff', 'inta'], [10, 10])
+#video.make_toggle(['diff', 'inta'], [20, 20])
 
 "gamma"
 #video.load_idea()
 #video.make_corr()
-#video.image_process_gamma(threshold=20)  
+#video.image_process_gamma()  
 #video.characterize_nps(save = False)
 ##video.info_add('\n--auto contrast--')
 ##video.info_add(video.auto_contrast)
-#video.exclude_nps([1.5], exclude = True)
-#video.make_toggle(['diff', 'corr'], [10, 10])
+#video.exclude_nps([1.2], exclude = True)
+##video.make_toggle(['diff', 'corr'], [10, 10])
+video.make_toggle(['diff', 'inta'], [20, 20])
 
 video.statistics()
 
